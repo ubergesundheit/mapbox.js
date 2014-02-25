4019e2d9e2a2b5b4d50e212e0117d5244152a2d8
tristen <tristen.brown@gmail.com>
2014-02-24 19:11:58 -0500

    use own defaults

--- assets/js/site.js
+++ assets/js/site.js
@@ -21,55 +21,6 @@
             prettyPrint(cb);
         },
 
-        bindSearch: function(input, menu) {
-            this.$el = input;
-            this.$menu = menu;
-            this.$el.on('keyup', $.proxy(this._keyup, this));
-        },
-
-        _keyup: function(e) {
-          switch(e.keyCode) {
-            case 40: // down arrow
-            case 38: // up arrow
-            case 13: // enter
-              break;
-            default:
-              this._search();
-          }
-          return false;
-        },
-
-        _search: function() {
-            var query = this.$el.val() ? this.$el.val().toLowerCase().match(/(\w+)/g) : null;
-
-            this.$menu.find('[href]').each(function() {
-                var $this = $(this),
-                    id = $this.attr('href').replace('#', '');
-
-                if (query) {
-                    _(query).each(function(q) {
-                        if (id.toLowerCase().indexOf(q) !== -1) {
-                            $this.show();
-                        } else {
-                            $this.hide();
-                        }
-                    });
-                } else {
-                    $this.show();
-                }
-            });
-
-            if (query) {
-                this.$menu.children().show();
-                this.$menu.children('.section').each(function() {
-                    var $this = $(this);
-                    if ($this.children(':visible').length < 2) $this.hide();
-                });
-            } else {
-                this.$menu.children().show();
-            }
-        },
-
         bindInlineCode: function() {
             var heading_index = {};
             function noparams(x) {
@@ -196,8 +147,6 @@ function load() {
         docs.colorCode();
     {% endif %}
 
-    docs.bindSearch($('#filter-api'), $('.js-nav-docs'));
-
     if (!window.location.origin) {
         window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
     }
@@ -226,9 +175,6 @@ function load() {
             });
         }
     });
-
-    var examples = new Docs();
-    examples.bindSearch($('#filter-examples'), $('.js-nav-examples'));
 }
 
 $(load);
